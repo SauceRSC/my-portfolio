fef85c15c12a57a78d3f431841a2f26475b888e5
Rodrigo Saucedo <rodrisc2000@hotmail.com>
2021-06-22 00:10:40 -0500

    Handles requests for /name returning my name

---
+++ portfolio/src/main/java/com/google/sps/servlets/NameServlet.java
@@ -0,0 +1,22 @@
+package com.google.sps.servlets;
+
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/** Handles requests sent to the /hello URL. Try running a server and navigating to /hello! */
+//mvn package exec:java
+
+@WebServlet("/name")
+public class NameServlet extends HttpServlet {
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    response.setContentType("text/html;");
+    response.getWriter().println("<h1>Jose Rodrigo Saucedo Cruz</h1>");
+  }
+}
+
+
