02bea4afdcb1069cc66c67b17790c8bba2022e75
Rodrigo Saucedo <rodrisc2000@hotmail.com>
2021-06-22 00:11:25 -0500

    Fetch the name from /name onload

--- portfolio/src/main/webapp/fetchScript.js
+++ portfolio/src/main/webapp/fetchScript.js
@@ -1,7 +1,12 @@
 async function showServerHello() {
-  const responseFromServer = await fetch('/hello');
+  const responseFromServer = await fetch('/name');
   const textFromResponse = await responseFromServer.text();
 
-  const dateContainer = document.getElementById('About');
-  dateContainer.appendChild(textFromResponse);
+  //get the h1 inside the about id in position 0 of the array
+  const dataContainer = document.getElementById('About').getElementsByTagName("h1");
+  //create a parser
+  var parser = new DOMParser();
+  //parse the text 
+  const text = parser.parseFromString(textFromResponse, "text/html");
+  dataContainer[0].innerText = text.body.innerText;
 }
